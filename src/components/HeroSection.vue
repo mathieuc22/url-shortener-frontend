4c375b5e691288ec8fd9a18f23f79651cc173e95
Mathieu <mathieu.collet@gmail.com>
2023-04-01 14:56:26 +0200

    Add hero section

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,86 @@
+<template>
+  <section class="hero">
+    <div class="container">
+      <div class="hero__content">
+        <h1 class="hero__title">Bienvenue sur LienCourt.fr</h1>
+        <p class="hero__subtitle">
+          Réduisez vos liens facilement et rapidement avec notre service.
+        </p>
+        <router-link to="/api-docs" class="hero__cta"
+          >Découvrez notre API</router-link
+        >
+      </div>
+      <img
+        class="hero__image"
+        src="/src/assets/hero.png"
+        alt="Collaboration et partage de liens courts"
+      />
+    </div>
+  </section>
+</template>
+
+<script setup>
+const name = "HeroSection";
+</script>
+
+<style scoped>
+.hero {
+  background-color: var(--color-secondary);
+  padding: 2rem 0;
+  text-align: center;
+}
+
+.hero__content {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+.hero__title {
+  font-family: var(--font-family-heading);
+  font-size: 2rem;
+  color: var(--color-light);
+  margin-bottom: 1rem;
+}
+
+.hero__subtitle {
+  font-size: 1rem;
+  color: var(--color-light);
+  margin-bottom: 2rem;
+}
+
+.hero__cta {
+  font-family: var(--font-family-heading);
+  font-size: 1rem;
+  background-color: var(--color-info);
+  color: var(--color-light);
+  padding: 0.5rem 1rem;
+  border-radius: var(--border-radius);
+  text-decoration: none;
+  transition: background-color var(--transition-duration);
+}
+
+.hero__cta:hover {
+  background-color: var(--color-info-darker);
+}
+
+.hero__image {
+  max-width: 100%;
+  height: auto;
+  margin-top: 2rem;
+}
+/* Styles pour les écrans plus larges */
+@media (min-width: 768px) {
+  .hero {
+    display: flex;
+    align-items: center;
+  }
+  .hero__content {
+    flex: 1;
+  }
+  .hero__image {
+    flex: 1;
+    margin-top: 0;
+  }
+}
+</style>
+  
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,3 +1,7 @@
 <template>
-    
+  <HeroSection />
 </template>
+
+<script setup>
+import HeroSection from "../components/HeroSection.vue";
+</script>

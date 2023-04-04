ce57be32c78fed66a9953cd33f5948958f2489e0
Mathieu <mathieu.collet@gmail.com>
2023-04-04 09:58:19 +0200

    Use axios

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,7 +2,8 @@
   <section class="contact">
     <div class="container">
       <h1>Contactez-nous</h1>
-      <form name="contact" class="contact-form" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+      <form name="contact" class="contact-form" method="POST" data-netlify="true" data-netlify-honeypot="bot-field"
+        @submit.prevent="handleSubmit">
         <input type="hidden" name="form-name" value="contact" />
         <div class="form-group">
           <label for="name">Nom</label>
@@ -16,13 +17,14 @@
           <label for="message">Message</label>
           <textarea id="message" name="message" v-model="message" required></textarea>
         </div>
-        <button class="button" type="submit" @click.prevent="handleSubmit">Envoyer</button>
+        <button class="button" type="submit">Envoyer</button>
       </form>
     </div>
   </section>
 </template>
-
+  
 <script setup>
+import axios from "axios";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 
@@ -31,22 +33,36 @@ const name = ref("");
 const email = ref("");
 const message = ref("");
 
+function encode(data) {
+  return Object.keys(data)
+    .map(
+      key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+    )
+    .join("&");
+}
+
 const handleSubmit = async (event) => {
   const formData = new FormData();
   formData.append("name", name.value);
   formData.append("email", email.value);
   formData.append("message", message.value);
 
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
+  const axiosConfig = {
+    header: { "Content-Type": "application/x-www-form-urlencoded" }
+  };
+  axios.post(
+    "/",
+    encode({
+      "form-name": "contact",
+      ...formData
+    }),
+    axiosConfig
+  )
     .then(() => router.push("/contact/success"))
-    .catch(() => router.push("/contact/error"));
-};
+    .catch(() => router.push("/contact/error"))
+}
 </script>
-
+  
 <style scoped>
 .contact {
   padding: 2rem 0;

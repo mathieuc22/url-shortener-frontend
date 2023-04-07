8da7f425d32737cb4a2d98da79f1e145e9d1b68b
Mathieu <mathieu.collet@gmail.com>
2023-04-07 11:13:43 +0200

    Add back to top button

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
   <Header></Header>
   <router-view></router-view>
+  <div class="back-to-top" v-show="isScrolled" @click="scrollToTop">
+    <font-awesome-icon icon="arrow-up" />
+  </div>
   <Footer></Footer>
 </template>
 
-<script>
+<script setup>
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
+import { ref, onBeforeUnmount } from "vue";
 
-export default {
-  components: {
-    Header,
-    Footer,
-  },
+const isScrolled = ref(false);
+
+const onScroll = () => {
+  isScrolled.value = window.scrollY > 300;
 };
+
+window.addEventListener("scroll", onScroll);
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+onBeforeUnmount(() => {
+  window.removeEventListener("scroll", onScroll);
+});
 </script>
+
+<style scoped>
+.back-to-top {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  background-color: var(--color-dark);
+  color: var(--color-light);
+  height: 35px;
+  width: 35px;
+  padding: 0;
+  border-radius: 50%;
+  cursor: pointer;
+  z-index: 1000;
+  transition: all 0.3s ease-in-out;
+  opacity: 0.8;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.back-to-top:hover {
+  background-color: var(--color-info);
+  opacity: 1;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,37 @@
-import { createApp } from 'vue';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faRocket, faLink, faChartBar, faSun, faMoon, faUserClock, faBars, faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import './style.css';
+import { createApp } from "vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faRocket,
+  faLink,
+  faChartBar,
+  faSun,
+  faMoon,
+  faUserClock,
+  faBars,
+  faTimes,
+  faEdit,
+  faArrowUp,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import "./style.css";
 
-import App from './App.vue';
-import router from './router';
+import App from "./App.vue";
+import router from "./router";
 
-library.add(faSun, faMoon, faRocket, faLink, faChartBar, faUserClock, faBars, faTimes, faEdit);
+library.add(
+  faSun,
+  faMoon,
+  faRocket,
+  faLink,
+  faChartBar,
+  faUserClock,
+  faBars,
+  faTimes,
+  faEdit,
+  faArrowUp
+);
 
 const app = createApp(App);
-app.component('font-awesome-icon', FontAwesomeIcon);
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(router);
-app.mount('#app');
+app.mount("#app");
